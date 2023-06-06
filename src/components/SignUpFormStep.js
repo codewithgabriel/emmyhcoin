858447c8823854a2a4a4a15bb1b228add1ccf685
codewithgabriel <olatoyeolaide@gmail.com>
2023-06-06 02:12:36 +0200

    modify buttons

--- src/components/SignUpFormStep.js
+++ src/components/SignUpFormStep.js
@@ -7,17 +7,16 @@ const SignUpFormStep = ({handleNextPrevClick}) => {
             <div style={{padding: 0 , margin: 0, paddingTop: 50}} className="container-fluid row col-12 container-form-card" >
 
 
-                    <div className="col-lg-6 col col-md-6 col-sm-12 col-xm-12" style={{padding: 0,  textAlign: 'center'}}>
-                         <div>
-                             <h2 style={{padding: 0, textAlign: 'center' }} className="form-title"></h2>
-                         </div>
+                    <div className="col-lg-6 col col-md-6 col-sm-12 col-xm-12 col-12" style={{ padding: 0,  textAlign: 'center'}}>
+
                         <div className="col-lg-3 col-md-3 col-sm-3 col-xm-12"  style={{margin: 'auto'}}>
                             <img src={signin} alt="sing" />
                         </div>
                     </div>
 
-                    <div className="col-lg-6 col col-md-6 col-sm-12 col-xm-12" style={{ padding: 0, textAlign: 'center' }}>
-                        <div>
+
+                    <div className="col-lg-6 col col-md-6 col-sm-12 col-xm-12 col-12" style={{ padding: 0, textAlign: 'center' }}>
+                        <div className="col-12">
                             <h2 className="form-title">Create Account Now</h2>
                             <p>Sometime fasinating awaits you, create account now to enjoy our satisfying services</p>
 
@@ -27,7 +26,10 @@ const SignUpFormStep = ({handleNextPrevClick}) => {
 
                         </div>
                     </div>
-                </div>
+
+
+
+            </div>
 
     );
 };
